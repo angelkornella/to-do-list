2adca63665ad0f034d2c7ef98d8807288c81080a
Angel Kornella Simamora <116823805+angelkornella@users.noreply.github.com>
2023-02-02 10:57:32 +0700

    Fitur login

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const login = document.getElementById("login");
 
+login.children[3].addEventListener("click", (e) => {
+    if (login.children[1].value === "admin" && login.children[2].value === "admin") {
+        window.location.href = "main.html";
+    }
+    else {
+        alert("Maaf, username atau password Anda tidak tepat.")
+    }
+})
